67eec484a6638fdc362c4d7f5483389ec9156188
Andrew <a.balanyuk@yahoo.com>
2016-11-01 21:20:22 +0200

    Initial commit

---
+++ app/api/openWeatherMap.jsx
@@ -0,0 +1,22 @@
+var axios = require('axios');
+
+
+const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?units=metric&APPID=91e673e403fb2cfe5b5f69b2b81c5ea1';
+
+module.exports = {
+  getTemp: function(location){
+    var encodedLocation = encodeURIComponent(location);
+    var requestUrl = `${OPEN_WEATHER_MAP_URL}&q=${location}`;
+
+    return axios.get(requestUrl).then((res)=>{
+      if(res.data.cod && res.data.message){
+        throw new Error(res.data.message);
+      } else {
+        return res.data.main.temp;
+      }
+    },(res)=>{
+      //reject
+      throw new Error(res.data.message);
+    });
+  }
+}
---
+++ app/components/WeatherForm.jsx
@@ -0,0 +1,26 @@
+var React = require('react');
+
+var WeatherForm = React.createClass({
+    onFormSubmit: function(e){
+      e.preventDefault();
+      var locationRef = this.refs.location;
+      var location = locationRef.value;
+
+      if(location.length > 0){
+        locationRef.value = '';
+        this.props.onSearch(location);
+      }
+    },
+    render: function(){
+      return(
+        <div>
+          <form onSubmit={this.onFormSubmit}>
+            <input type="text" placeholder="Enter city name" ref="location"/>
+            <button>Get Weather</button>
+          </form>
+        </div>
+      );
+    }
+});
+module.exports = WeatherForm;
+//http://api.openweathermap.org/data/2.5/weather?q=London,uk&units=metric&APPID=91e673e403fb2cfe5b5f69b2b81c5ea1
