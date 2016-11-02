b16d0db1d880c5fa2e14628591228b5a0dbf7fd8
Andrew <a.balanyuk@yahoo.com>
2016-11-02 12:02:54 +0200

    Style about page & link to resources

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,22 @@ var React = require('react');
 var About = (props) => {
   return (
     <div>
-      <h1>About</h1>
-      <p>Welcome to about page!</p>
+      <h1 className="text-centered">About</h1>
+      <p>Welcome to weather app!</p>
+      <p>This is a weather app I've built
+        for the Complete React Web App Dev Course.</p>
+      <p>You can get realtime temperature data for any city in the world!</p>
+      <p>Here are some of the tools I've used</p>
+      <ul>
+        <li>
+          <a href="https://facebook.github.io/react">React</a>
+        </li>
+        <li>
+          <a href="http://openweathermap.org">Open Weather Map</a> - Weather data API provider
+        </li>
+      </ul>
+      <p><b>Source code for the project</b></p>
+      <p>Github: <a href="https://github.com/aikaramba/sturdy-broccoli">https://github.com/aikaramba/sturdy-broccoli</a></p>
     </div>
   );
 };
